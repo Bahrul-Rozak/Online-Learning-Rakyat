a775e9d871333e9a70a457cbccc2d938ccb2f509
Bahrul Rozak <bahrulrozak13062001@gmail.com>
2023-03-02 09:24:32 +0700

    Services Component 2

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -3,6 +3,8 @@ import { Col, Container, Row } from "react-bootstrap";
 import designIcon from "../../asset/image/design.png";
 import ecommerceIcon from "../../asset/image/ecommerce.png";
 import webIcon from "../../asset/image/web.png";
+import "../../asset/css/custom.css";
+import "../../asset/css/bootstrap.min.css";
 
 class Services extends Component {
   render() {
@@ -11,23 +13,41 @@ class Services extends Component {
         <Container>
           <Row>
             <Col lg={4} md={6} sm={12}>
-              <div>
+              <div className="serviceCard text-center">
                 <img
                   className="designIcon"
                   src={designIcon}
                   alt="design-icon"
                 />
+                <h2 className="serviceName">Web Design</h2>
+                <p className="serviceDescription">
+                  Qualified web design and attractive effects which catches
+                  visitor's eye
+                </p>
               </div>
             </Col>
             <Col lg={4} md={6} sm={12}>
-              <img
-                className="ecommerceIcon"
-                src={ecommerceIcon}
-                alt="ecommerce-icon"
-              />
+              <div className="serviceCard text-center">
+                <img
+                  className="ecommerceIcon"
+                  src={ecommerceIcon}
+                  alt="ecommerce-icon"
+                />
+                <h2 className="serviceName">E-Commerce</h2>
+                <p className="serviceDescription">
+                  I will design and develop ecommerce online store website
+                </p>
+              </div>
             </Col>
             <Col lg={4} md={6} sm={12}>
-              <img className="webIcon" src={webIcon} alt="web-icon" />
+              <div className="serviceCard text-center">
+                <img className="webIcon" src={webIcon} alt="web-icon" />
+                <h2 className="serviceName">Web Development</h2>
+                <p className="serviceDescription">
+                  Clean and issues free code to make your website dynamic
+                  perfectly
+                </p>
+              </div>
             </Col>
           </Row>
         </Container>
