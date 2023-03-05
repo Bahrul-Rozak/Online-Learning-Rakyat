898b5d1ab02fd203dd8699dd2ffa81cbc71aebb1
Bahrul Rozak <bahrulrozak13062001@gmail.com>
2023-03-05 15:56:37 +0700

    Summary Component 3

--- src/components/Summary/Summary.jsx
+++ src/components/Summary/Summary.jsx
@@ -1,5 +1,5 @@
 import React, { Component, Fragment } from "react";
-import { Col, Container, Row } from "react-bootstrap";
+import { Card, Col, Container, Row } from "react-bootstrap";
 
 class Summary extends Component {
   render() {
@@ -14,25 +14,37 @@ class Summary extends Component {
                     <Col>
                       <h1 className="countNumber">3000 </h1>
                       <h4 className="countTitle">Student Worldwide</h4>
-                      <hr className="bg-white w-25" />
+                      <hr className="bg-white w-30 h-15" />
                     </Col>
 
                     <Col>
                       <h1 className="countNumber">3000 </h1>
                       <h4 className="countTitle">Student Worldwide</h4>
-                      <hr className="bg-white w-25" />
+                      <hr className="bg-white w-30 h-15" />
                     </Col>
 
                     <Col>
                       <h1 className="countNumber">3000 </h1>
                       <h4 className="countTitle">Student Worldwide</h4>
-                      <hr className="bg-white w-25" />
+                      <hr className="bg-white w-25 h-15" />
                     </Col>
                   </Row>
                 </Col>
 
                 <Col lg={4} md={6} sm={12}>
-                  <h1>FOUR </h1>
+                  <Card className="workCard">
+                    <Card.Body>
+                      <Card.Title className="cardTitle">
+                        What I Have Achieved
+                      </Card.Title>
+                      <Card.Text>
+                        <p className="cardSubTitle">Requirement Gathering</p>
+                        <p className="cardSubTitle">System Analysis</p>
+                        <p className="cardSubTitle">Coding Testing</p>
+                        <p className="cardSubTitle">Implementation</p>
+                      </Card.Text>
+                    </Card.Body>
+                  </Card>
                 </Col>
               </Row>
             </Container>
